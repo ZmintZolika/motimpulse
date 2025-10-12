c798c95f442330dcd3a85bbd1b1093aa5744d958
ZmintZolika <bodvanszki@gmail.com>
2025-10-12 17:55:54 +0200

    API routes: DayEntry végpontok (CRUD műveletek napi bejegyzésekhez)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DayEntryController;
 
 // Nyilvános útvonalak (nem kell bejelentkezés)
 Route::post('/register', [AuthController::class, 'register']);
@@ -11,4 +12,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
+});
+
+
+// Day Entries
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/day-entries', [DayEntryController::class, 'index']);
+    Route::post('/day-entries', [DayEntryController::class, 'store']);
+    Route::get('/day-entries/{dayEntry}', [DayEntryController::class, 'show']);
+    Route::put('/day-entries/{dayEntry}', [DayEntryController::class, 'update']);
+    Route::delete('/day-entries/{dayEntry}', [DayEntryController::class, 'destroy']);
 });
