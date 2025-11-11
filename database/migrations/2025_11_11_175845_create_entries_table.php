974e40dd46a7099b9b8352f0f67b2821ef666de2
Viktor <szerencsesvik@gmail.com>
2025-11-11 19:10:34 +0100

    Adatbázis létrehozása

---
+++ database/migrations/2025_11_11_175845_create_entries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entries', function (Blueprint $table) {
+        $table->id('entry_id');
+        $table->unsignedBigInteger('user_id');
+        $table->unsignedBigInteger('quote_id')->nullable();
+        $table->enum('mood', ['Lehangolt', 'Kiegyensúlyozott', 'Vidám']);
+        $table->enum('weather', ['Napos', 'Felhős', 'Esős', 'Szeles', 'Havas']);
+        $table->enum('sleep_quality', ['Nagyon rossz', 'Rossz', 'Közepes', 'Jó', 'Kiváló']);
+        $table->enum('activities', ['Munka', 'Tanulás', 'Pihenés', 'Sport', 'Szórakozás', 'Egyéb']);
+        $table->enum('health_action', ['Mozgás', 'Egészséges táplálkozás', 'Pihenés', 'Semmi']);
+        $table->text('note')->nullable();
+        $table->boolean('is_deleted')->default(false);
+        $table->timestamp('created_at')->nullable();
+        $table->timestamp('updated_at')->nullable();
+        $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entries');
+    }
+};
