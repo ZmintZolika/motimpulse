8eb49bb7cd7e59bbb9cb83c6e66b57bb6f05bfb6
ZmintZolika <bodvanszki@gmail.com>
2025-11-18 22:11:31 +0100

    fix: QuoteSeeder osztálynév javítása

--- database/seeders/QuoteSeeder.php
+++ database/seeders/QuoteSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Quote;
 
-class MotivationalQuoteSeeder extends Seeder
+class QuoteSeeder extends Seeder
 {
     public function run(): void
     {
