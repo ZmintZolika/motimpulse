f22f6225bf18dc0e31d7f2ec7335f8b60c7932a7
ZmintZolika <bodvanszki@gmail.com>
2025-11-19 17:39:05 +0100

    fix: API exception handler JSON válaszokhoz
    - 404 és 405 hibák JSON formátumban (nem HTML)
    - Accept: application/json header támogatás
    - Validációs hibák helyes kezelése (422)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,35 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php', 
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        // API route-ok JSON választ adnak
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Endpoint nem található.'
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Hibás HTTP method.'
+                ], 405);
+            }
+        });
     })->create();
