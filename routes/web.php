4b2af68955a2bfdf17a29b0bb625022c1d2464f6
MSerpentine <koncz.nelly@gmail.com>
2025-11-21 21:44:14 +0100

    Update regisztracios oldal

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+use Illuminate\Http\Request;
+
+
+Route::get('/', function () {
+    return view('/home');});
+
+Route::view('/', 'home')->name('home');
+Route::view('/login', 'login')->name('login');
+Route::view('/register', 'register')->name('register');
+Route::view('/naplo', 'day-entries')->name('day-entries');
+
+//---------------------------------------------------------
+
+Route::post('/set-token', function (Request $request) {
+    session(['auth_token' => $request->token]);
+    return response()->json(['message' => 'Token stored']);
+});
+
+Route::get('/teszt', function () {
+    return 'Működik!';
+});
+
+Route::get('/teszt-token', function () {
+    return session('auth_token') ?? 'Nincs token';
+});
